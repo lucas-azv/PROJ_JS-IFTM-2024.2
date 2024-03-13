cdf80def6af4e8c38cc28204a6635a96b939d8bf
Lucas <108193174+lucas-azv@users.noreply.github.com>
2024-03-13 19:39:34 -0300

    Add files via upload

--- 06.03.24/lista 2/script.js
+++ 06.03.24/lista 2/script.js
@@ -1,6 +1,30 @@
-window.addEventListener('load', function(){
-    
-    var vet = [12,8,25,54,14,18,84,22,10]
+window.addEventListener('load', function () {
 
-    
+    var idades = [12, 8, 25, 54, 14, 18, 84, 22, 10]
+    parseInt(idadeInput = prompt("insira uma idade qualquer"))
+
+    somaIdades = idades.reduce((acc, idade) => acc + idade, 0);
+    document.write(`<p>A soma das idades é: ${somaIdades}</p>`);
+
+    mediaIdades = somaIdades / idades.length;
+    document.write(`<p>A média das idades é: ${parseFloat(mediaIdades)}</p>`);
+
+    maiorIdade = idades.reduce((acc, idade) => Math.max(acc, idade), 0);
+    document.write(`<p>A maior idade é: ${maiorIdade}</p>`);
+
+    idadesImpares = idades.filter(idade => idade % 2 !== 0);
+    document.write(`<p>As idades ímpares são: ${idadesImpares.join(', ')}</p>`);
+
+    todosMaioresDeIdade = idades.every(idade => idade >= 18);
+    document.write(`<p>Todos são maiores de idade? ${todosMaioresDeIdade}</p>`);
+
+    todasMaioresIguaisValor = idades.every(idade => idade >= idadeInput);
+    document.write(`<p>Todas as idades são maiores ou iguais a ${idadeInput}? ${todasMaioresIguaisValor}</p>`);
+
+    idadesMaioresIguais = idades.filter(idade => idade >= idadeInput);
+    document.write(`<p>As idades maiores ou iguais a ${idadeInput} são: ${idadesMaioresIguais.join(', ')}</p>`);
+
+    mediaIdadesMaioresIguais = idadesMaioresIguais.reduce((acc, idade) => acc + idade, 0) / idadesMaioresIguais.length;
+    document.write(`<p>A média das idades das pessoas com idade maior ou igual a ${idadeInput} é: ${mediaIdadesMaioresIguais}</p>`);
+
 })
