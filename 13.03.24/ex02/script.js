2a3ab7e53f49f8ef2f6ce2b764aff51f6edaca5b
Lucas <108193174+lucas-azv@users.noreply.github.com>
2024-03-13 22:27:08 -0300

    Add files via upload

--- 13.03.24/ex02/script.js
+++ 13.03.24/ex02/script.js
@@ -1,8 +1,13 @@
 document.getElementById("cep").addEventListener("blur", () => {
     var cep = document.getElementById("cep").value;
 
-    if(cep.length = 8){
+    if(cep.length == 8){
         fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(response => response.json)
+        .then(conteudo => {
+            document.getElementById("rua").value = conteudo.logradouro;
+            document.getElementById("cidade").value = conteudo.localidade;
+        })
     } else{
         alert("MORRAAAAAAAAA AAAAAAAAAAAAAAAAAAAAAAAAAARRRRRRRGGGGGGGHHHHHHHHH")
     }
