8e667ef7ad8bd7a00e03ddff393c6e228ff0ecdf
Lucas <108193174+lucas-azv@users.noreply.github.com>
2024-03-26 01:20:59 -0300

    Add files via upload

--- tmdb-api/js/script.js
+++ tmdb-api/js/script.js
@@ -17,24 +17,44 @@ window.addEventListener('load', () => {
     function exibir(filmes) {
         filmes.slice(0,10).forEach(filme => {
             var boxFilme = document.createElement("div");
+            boxFilme.id = "box-filme";
+
             var tituloFilme = document.createElement("p");
             var poster = document.createElement("img");
-            var nota = document.createElement("p")
+            var nota = document.createElement("p");
+            var sinopse = document.createElement("p");
+            // var lancamento = document.createElement("p");
+
             var url = "https://image.tmdb.org/t/p/w500";
 
+            var info = document.createElement("div");
+            info.id = "info";
+
             tituloFilme.id = "tituloFilme"
+            nota.id = "nota";
 
             poster.setAttribute("src", `${url}${filme.poster_path}`);
 
             tituloFilme.textContent = filme.original_title;
-            nota.textContent = (`⭐${filme.vote_average.toFixed(1)}`);
+            nota.textContent = (`⭐ ${filme.vote_average.toFixed(1)}`);
+            sinopse.textContent = filme.overview;
+            // lancamento.textContent = (`📅 ${filme.release_date}`);
 
             boxFilme.appendChild(poster);
             boxFilme.appendChild(tituloFilme);
             boxFilme.appendChild(nota);
+            boxFilme.appendChild(sinopse);
 
             
             document.getElementById("box-filmes").appendChild(boxFilme);
+
+            info.appendChild(tituloFilme);
+            // info.appendChild(lancamento);
+            info.appendChild(nota);
+            info.appendChild(sinopse);
+
+            boxFilme.appendChild(info);
+            
         });
     }
 })
