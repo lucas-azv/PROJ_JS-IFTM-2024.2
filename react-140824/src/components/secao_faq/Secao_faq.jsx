baac2ce690b2f47cf8183a8badb255893cf81d31
Lucas <108193174+lucas-azv@users.noreply.github.com>
2024-08-14 22:42:21 -0300

    Add files via upload

--- react-140824/src/components/secao_faq/Secao_faq.jsx
+++ react-140824/src/components/secao_faq/Secao_faq.jsx
@@ -6,19 +6,22 @@ function Secao_faq() {
 
     useEffect(() => {
 		fetch('https://wilton-filho.github.io/PFJS-GitHub/React/projeto/json/faq.json')
-    .then(file => file.json)
+    .then(file => file.json())
     .then(info => setFaqs(info.faqs))
 	}, []);
 
     function getPerguntas(){
-        return (
-            <details open>
-                <summary>Pergunta 1</summary>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique voluptate atque provident corrupti laudantium aut iusto repudiandae? Laborum consequatur molestiae eveniet praesentium, similique beatae rem recusandae necessitatibus libero ratione voluptate?</p>
-            </details>
-        )
+        if(faqs.length > 0){
+            return(
+                faqs.map((faq, index) =>
+                <details key={index}>
+                    <summary>{faq.pergunta}</summary>
+                    <p>{faq.resposta}</p>
+                </details>
+                )
+            )
+        }
     }
-
     return (
         <section>
             <h2>FAQ</h2>
